a26afb81303f08a812bdb149f5e1a7e304c9f729
Berrie Kremers <33800564+katoemba@users.noreply.github.com>
2020-11-05 21:46:54 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "LastFMApi",
-    platforms: [.macOS(.v10_11), .iOS(.v10), .tvOS(.v9), .watchOS(.v3)],
+    platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v9), .watchOS(.v3)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
